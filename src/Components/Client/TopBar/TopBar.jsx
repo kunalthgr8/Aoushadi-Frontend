cdc81c38b17ad214076d41adec032e914990d863
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-01-28 14:59:30 +0530

    Did Nothing

--- src/Components/Client/TopBar/TopBar.jsx
+++ src/Components/Client/TopBar/TopBar.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Account from "../../../Assets/Account.svg";
 import "./TopBar.css";
 import { NavLink } from "react-router-dom";
 
-import {  useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+// import Example from "./Menu/Menu";
 
 const TopBar = ({ username }) => {
   const location = useLocation();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
 
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const isUserActive = location.pathname === "/user";
-  
+
   return (
     <>
-      {!isUserActive && (
-        <div className="TopBar">
+      <div className={windowWidth < 761 ? "topbar" : "TopBar"}>
+        {windowWidth < 761 && <div>Hello </div>}
+        {!isUserActive && (
           <div className="Image-TopBar">
             <NavLink to="/user">
               <img src={Account} alt="Account" />
             </NavLink>
             <p>{username}</p>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
